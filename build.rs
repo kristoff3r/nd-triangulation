bb3b878b40d4ecc885ac38aaa701ac115bb45573
Kristoffer Søholm <k.soeholm@gmail.com>
2020-05-21 01:08:32 +0200

    Fix C++ build on Debian 11

--- build.rs
+++ build.rs
@@ -3,11 +3,11 @@ fn main() {
     cpp_build::Config::new()
         .compiler("/usr/bin/g++")
         .flag("-frounding-math")
+        .flag("-std=c++14")
         .include("/usr/include/eigen3")
         .build("src/lib.rs");
 
     println!("cargo:rustc-link-search=/usr/lib");
-    println!("cargo:rustc-link-lib=CGAL");
     println!("cargo:rustc-link-lib=gmp");
     println!("cargo:rustc-link-lib=mpfr");
 }
