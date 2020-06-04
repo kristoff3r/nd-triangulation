f56049ed839bc8f305b79bc32e52629ab40512e5
Kristoffer Søholm <k.soeholm@gmail.com>
2020-06-04 22:55:00 +0200

    Made build.rs NixOS compatible

--- build.rs
+++ build.rs
@@ -1,13 +1,15 @@
 #[cfg(not(feature = "docs-rs"))]
 fn main() {
-    cpp_build::Config::new()
-        .compiler("/usr/bin/g++")
-        .flag("-frounding-math")
-        .flag("-std=c++14")
-        .include("/usr/include/eigen3")
-        .build("src/lib.rs");
+    let eigen = pkg_config::Config::new().atleast_version("3.3.0").probe("eigen3").unwrap();
+    let mut conf = cpp_build::Config::new();
+    conf.flag("-frounding-math");
+    for path in eigen.include_paths {
+        conf.include(path);
+    }
+    conf.build("src/lib.rs");
 
     println!("cargo:rustc-link-search=/usr/lib");
+    println!("cargo:rustc-link-lib=CGAL");
     println!("cargo:rustc-link-lib=gmp");
     println!("cargo:rustc-link-lib=mpfr");
 }
